f637d76b7358374ef914fef6ec56ac46bcb06caa
DineshKuna <kunadinesh.work@gmail.com>
2024-10-28 17:24:26 +0530

    version10

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./components/home/Home";
-// import Favourite from "./components/favourite/Favourite";
-// import NavBar from "./components/navbar/NavBar";
-// import OneProduct from "./components/oneProduct/OneProduct";
-
-// function App() {
-//   return (
-   
-//     <Router>
-//        <NavBar />
-//       <div className="App">
-        
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/favourite" element={<Favourite />} />
-//           <Route path="/oneproduct/:id" element={<OneProduct />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar.jsx";
 import OneProduct from "./components/oneProduct/OneProduct.jsx";
